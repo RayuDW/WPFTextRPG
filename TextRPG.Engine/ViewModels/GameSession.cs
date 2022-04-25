262b1d3a34b795879bb69f6b1626ac0c5d0236f3
RayuDW <51034418+RayuDW@users.noreply.github.com>
2022-04-25 21:54:40 +0200

    Created the Player Model

---
+++ TextRPG.Engine/ViewModels/GameSession.cs
@@ -0,0 +1,17 @@
+﻿using TextRPG.Engine.Models;
+
+namespace TextRPG.Engine.ViewModels;
+
+public class GameSession
+{
+    private Player CurrentPlayer { get; set; }
+
+    public GameSession()
+    {
+        CurrentPlayer = new Player
+        {
+            Name = "Rayu",
+            Gold = 100000
+        };
+    }
+}
