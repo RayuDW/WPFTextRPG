79f26b2661c75a7b2b51a601d19840fd1c88c6fe
RayuDW <51034418+RayuDW@users.noreply.github.com>
2022-04-26 14:37:30 +0200

    Added Location.cs class

--- TextRPG.Engine/ViewModels/GameSession.cs
+++ TextRPG.Engine/ViewModels/GameSession.cs
@@ -5,6 +5,7 @@ namespace TextRPG.Engine.ViewModels;
 public class GameSession
 {
     public Player CurrentPlayer { get; set; }
+    public Location CurrentLocation { get; set; }
 
     public GameSession()
     {
@@ -16,6 +17,15 @@ public class GameSession
             Gold = 100000,
             ExperiencePoints = 0,
             Level = 1
+        };
+
+        CurrentLocation = new Location
+        {
+            XCoordinate = 0,
+            YCoordinate = -1,
+            Name = "Home",
+            Description = "This is your home! Go and cuddle someone!",
+            ImageName = "/TextRPG.Engine;component/Images/Home.png"
         };
     }
 }
